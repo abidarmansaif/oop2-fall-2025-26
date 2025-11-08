459b78e04306f504b2989df27489cf02685f56ff
abidarmansaif <abidarmansaif@gmail.com>
2025-11-09 00:02:37 +0600

    23-55624-3

---
+++ task3/task3/task3/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter the temperature in Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+double fahrenheit = (celsius * 9 / 5) + 32;
+Console.WriteLine($"Temperature in Fahrenheit: {fahrenheit}");
