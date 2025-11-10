71cb3d20817098162f090e9150b4c0594b9f5bee
abidarmansaif <abidarmansaif@gmail.com>
2025-11-10 15:02:02 +0600

    23-55624-3

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+int s1 = Convert.ToInt32(Console.ReadLine());
+int s2 = Convert.ToInt32(Console.ReadLine());
+int s3 = Convert.ToInt32(Console.ReadLine());
+int sum = s1 + s2 + s3;
+
+double average = sum / 3.0;  // use 3.0 for correct decimal division
+
+Console.WriteLine("Sum: " + sum);
+
+Console.WriteLine("Average: " + average);
+
